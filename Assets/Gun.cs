c344710919cedfb9b84dc15245307ed60c6333a1
Clay Ewing <c.ewing@miami.edu>
2017-11-09 15:17:02 -0500

    Initial commit

---
+++ Assets/Gun.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour {
+	public GameObject bullet;
+	public GameObject target;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire1")) {
+
+			RaycastHit hit;
+			if (Physics.Raycast (transform.position, transform.right * -1f, out hit)) {
+				GameObject newObj = (GameObject)Instantiate (bullet);
+				target = hit.collider.gameObject;
+				newObj.GetComponent<Snowball> ().thingIFollow = target;			
+				newObj.transform.position = transform.position;
+
+			} else {
+				GameObject newObj = (GameObject)Instantiate (bullet);
+				newObj.transform.position = transform.position;
+				newObj.transform.rotation = transform.rotation;
+			}
+
+
+		}
+	}
+}
---
+++ Assets/Snowball.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snowball : MonoBehaviour {
+	public GameObject thingIFollow;
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (thingIFollow != null) {
+			transform.position = Vector3.MoveTowards (transform.position, thingIFollow.transform.position, Time.deltaTime * speed);
+		} else {
+			transform.Translate (transform.forward * 1.5f);
+//			transform.position = Vector3.MoveTowards (transform.position, transform.position + transform.forward, Time.deltaTime);
+		}
+	}
+}
